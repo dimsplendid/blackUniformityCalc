5e752213adbd729a1b5169605c1fbb018859411f
Dim.splendid <dimsplendid@gmail.com>
2018-11-22 08:35:30 +0800

    batch add items' name

--- tools/batch/batch.cmd
+++ tools/batch/batch.cmd
@@ -12,7 +12,8 @@ set x2=%3
 set y2=%4
 
 del ouput\out.csv /F /Q
-
+:: items name
+echo ID,BlackU,BlackU_cut,x1,y1,x2,y2,x3,y3,x4,y4,x5,y5,x6,y6,comment >> output\out.csv
 for %%f in (*.csv) do (
     echo %%~nf
     BlackMura_win "%%~nf.csv" %x1% %y1% %x2% %y2% >> output\out.csv
